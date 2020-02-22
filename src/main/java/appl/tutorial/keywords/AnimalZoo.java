bb4551b46836b5582fcf973223090f58e1ab90a3
Marc Leonhard <leonhard.marc@web.de>
2020-02-22 12:36:48 +0100

    =test...

--- src/main/java/appl/tutorial/keywords/AnimalZoo.java
+++ src/main/java/appl/tutorial/keywords/AnimalZoo.java
@@ -3,7 +3,6 @@ package appl.tutorial.keywords;
 public class AnimalZoo {
 
 	public static void main(String[] args) {
-		// bla
 		Object xxx = new Animal("tiger");
 		if (xxx instanceof Animal) {
 			System.out.println("tiger is an animal");

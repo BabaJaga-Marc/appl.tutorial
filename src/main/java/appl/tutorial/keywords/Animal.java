97c81b5f822b2de8e78a4ea1069d73fc8f8756dc
leonh <leonhard.marc@web.de>
2020-02-24 19:36:10 +0100

    add animal to  animalzoo

--- src/main/java/appl/tutorial/keywords/Animal.java
+++ src/main/java/appl/tutorial/keywords/Animal.java
@@ -3,13 +3,41 @@ package appl.tutorial.keywords;
 public class Animal {
 
 	private String type;
+	private String name;
+	private int age;
 
 	public Animal(String type) {
 		this.type = type;
 	}
 
+	public Animal(String type, String name, int age) {
+		this.type = type;
+		this.name = name;
+		this.age = age;
+	}
+
+	public String getAllInfo() {
+		return type + ", " + name + "," + age;
+	}
+
 	public String getType() {
 		return type;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
 	}
 
 }
--- src/main/java/appl/tutorial/keywords/AnimalZoo.java
+++ src/main/java/appl/tutorial/keywords/AnimalZoo.java
@@ -1,6 +1,15 @@
 package appl.tutorial.keywords;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class AnimalZoo {
+
+	private List<Animal> animalList = new ArrayList<Animal>();
+
+	public void addAnimal(Animal animal) {
+		animalList.add(animal);
+	}
 
 	public static void main(String[] args) {
 		Object xxx = new Animal("tiger");
@@ -10,5 +19,27 @@ public class AnimalZoo {
 			System.out.println("tiger is not an animal");
 		}
 
+		AnimalZoo animalZoo = new AnimalZoo();
+
+		Animal ape = new Animal("ape");
+		ape.setName("Igo");
+		ape.setAge(5);
+		animalZoo.addAnimal(ape);
+		animalZoo.addAnimal(new Animal("turtle", "mimi", 110));
+		// animalZoo.printAllMyAnimals();
+		animalZoo.printAllMyAnimalsOlderThan(2);
+	}
+
+	private void printAllMyAnimalsOlderThan(int olderThan) {
+		for (Animal animal : animalList) {
+			if (animal.getAge() > olderThan)
+				System.out.println(animal.getAllInfo());
+		}
+	}
+
+	public void printAllMyAnimals() {
+		for (Animal animal : animalList) {
+			System.out.println(animal.getType() + ", " + animal.getName());
+		}
 	}
 }
